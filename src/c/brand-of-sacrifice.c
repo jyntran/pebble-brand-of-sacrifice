fa1d7bd40225249a0b6efbe4e85ed30700d51a67
Jen Tran <jen.tran@uwaterloo.ca>
2016-10-27 13:16:33 -0400

    Designed time

--- src/c/brand-of-sacrifice.c
+++ src/c/brand-of-sacrifice.c
@@ -1,15 +1,20 @@
 #include <pebble.h>
+#include <string.h>
 
 static Window *s_window;
-static TextLayer *s_text_layer;
+static TextLayer *s_text_layer_h, *s_text_layer_m;
 
 static void update_time() {
   time_t temp = time(NULL);
   struct tm *tick_time = localtime(&temp);
-  static char s_buffer[8];
-  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
-                                          "%H:%M" : "%I:%M", tick_time);
-  text_layer_set_text(s_text_layer, s_buffer);
+  static char s_buffer_hour[8];
+  strftime(s_buffer_hour, sizeof(s_buffer_hour), clock_is_24h_style() ?
+                                          "%H" : "%I", tick_time);
+  static char s_buffer_mins[8];
+  strftime(s_buffer_mins, sizeof(s_buffer_mins), "%M", tick_time);
+  
+  text_layer_set_text(s_text_layer_h, s_buffer_hour);
+  text_layer_set_text(s_text_layer_m, s_buffer_mins);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -20,15 +25,24 @@ static void prv_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  s_text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
-  text_layer_set_background_color(s_text_layer, GColorClear);
-  text_layer_set_text_color(s_text_layer, GColorRed);
-  text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
+  s_text_layer_h = text_layer_create(GRect(0, 72, bounds.size.w, 28));
+  text_layer_set_background_color(s_text_layer_h, GColorClear);
+  text_layer_set_text_color(s_text_layer_h, GColorRed);
+  text_layer_set_font(s_text_layer_h, fonts_get_system_font(FONT_KEY_LECO_28_LIGHT_NUMBERS));
+  text_layer_set_text_alignment(s_text_layer_h, GTextAlignmentLeft);
+  layer_add_child(window_layer, text_layer_get_layer(s_text_layer_h));
+
+  s_text_layer_m = text_layer_create(GRect(0, 72, bounds.size.w, 28));
+  text_layer_set_background_color(s_text_layer_m, GColorClear);
+  text_layer_set_text_color(s_text_layer_m, GColorRed);
+  text_layer_set_font(s_text_layer_m, fonts_get_system_font(FONT_KEY_LECO_28_LIGHT_NUMBERS));
+  text_layer_set_text_alignment(s_text_layer_m, GTextAlignmentRight);
+  layer_add_child(window_layer, text_layer_get_layer(s_text_layer_m));
 }
 
 static void prv_window_unload(Window *window) {
-  text_layer_destroy(s_text_layer);
+  text_layer_destroy(s_text_layer_h);
+  text_layer_destroy(s_text_layer_m);
 }
 
 static void prv_init(void) {
