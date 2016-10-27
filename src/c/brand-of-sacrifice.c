57d5aaa5572e7fb8d5119a2e7fe43dd314101b99
Jen Tran <jen.tran@uwaterloo.ca>
2016-10-27 11:37:21 -0400

    Added time, updated package

--- src/c/brand-of-sacrifice.c
+++ src/c/brand-of-sacrifice.c
@@ -3,22 +3,17 @@
 static Window *s_window;
 static TextLayer *s_text_layer;
 
-static void prv_select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Select");
+static void update_time() {
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
+                                          "%H:%M" : "%I:%M", tick_time);
+  text_layer_set_text(s_text_layer, s_buffer);
 }
 
-static void prv_up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Up");
-}
-
-static void prv_down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Down");
-}
-
-static void prv_click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, prv_select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, prv_up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, prv_down_click_handler);
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
 }
 
 static void prv_window_load(Window *window) {
@@ -26,7 +21,8 @@ static void prv_window_load(Window *window) {
   GRect bounds = layer_get_bounds(window_layer);
 
   s_text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
-  text_layer_set_text(s_text_layer, "Press a button");
+  text_layer_set_background_color(s_text_layer, GColorClear);
+  text_layer_set_text_color(s_text_layer, GColorRed);
   text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
 }
@@ -36,17 +32,21 @@ static void prv_window_unload(Window *window) {
 }
 
 static void prv_init(void) {
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+
   s_window = window_create();
-  window_set_click_config_provider(s_window, prv_click_config_provider);
   window_set_window_handlers(s_window, (WindowHandlers) {
     .load = prv_window_load,
     .unload = prv_window_unload,
   });
   const bool animated = true;
   window_stack_push(s_window, animated);
+  window_set_background_color(s_window, GColorBlack);
+  update_time();
 }
 
 static void prv_deinit(void) {
+  tick_timer_service_unsubscribe();
   window_destroy(s_window);
 }
 
